b2cf360b3a9689e7b69485d78938c9a76e29415e
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-13 13:47:12 -0500

    feat: devnet from cardano-scaling/hydra repo
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- update.sh
+++ update.sh
@@ -2,10 +2,33 @@
 
 cd config
 for network in sanchonet preview preprod mainnet; do
+	mkdir -p $network
 	cd $network && \
 		for filename in config.json config-bp.json topology.json {byron,shelley,alonzo,conway}-genesis.json; do
-       			curl -sLo $filename https://book.play.dev.cardano.org/environments/$network/$filename
-			sed -i -e 's/127.0.0.1/0.0.0.0/' $filename
+			curl -sL https://book.play.dev.cardano.org/environments/$network/$filename | sed \
+				-e 's/127.0.0.1/0.0.0.0/' > $filename
 		done
 	cd ..
 done
+
+if [[ ${HAIL_HYDRA:-false} ]]; then
+	network=devnet
+	baseurl=https://raw.githubusercontent.com/cardano-scaling/hydra/refs/heads/master/hydra-cluster/config
+	mkdir -p $network
+	cd $network && \
+		curl -sL $baseurl/$network/cardano-node.json | sed \
+			-e 's/genesis-byron/byron-genesis/g' \
+			-e 's/genesis-shelley/shelley-genesis/g' \
+			-e 's/genesis-alonzo/alonzo-genesis/g' \
+			-e 's/genesis-conway/conway-genesis/g' \
+		> config.json
+		for genesis in byron shelley alonzo conway; do
+			curl -sLo $genesis-genesis.json $baseurl/$network/genesis-$genesis.json
+		done
+		mkdir -p keys && cd keys
+		for filename in kes.skey vrf.skey opcert.cert byron-delegat{ion.cert,e.key}; do
+			curl -sLo $filename $baseurl/$network/$filename
+		done
+		cd ..
+	cd ..
+fi
